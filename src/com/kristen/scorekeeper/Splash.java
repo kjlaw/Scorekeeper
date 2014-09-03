5502b1edbac8229cd591e354d00dc6cf5279f30e
Kristen <kristenlaw.2014@gmail.com>
2014-09-03 15:09:13 -0400

    Added splash screen

---
+++ src/com/kristen/scorekeeper/Splash.java
@@ -0,0 +1,26 @@
+package com.kristen.scorekeeper;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends Activity {
+	
+	private Intent myintent;
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+
+		myintent = new Intent(this, MainActivity.class);
+
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				startActivity(myintent);
+				finish();
+			}
+		}, 1000);
+	}
+}
